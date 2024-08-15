d4c960c7b0e351da06e71ea5bc6b04ff39c8b787
Clagarnach <olieclay@hotmail.com>
2024-08-15 15:52:28 +0100

    Add Dockerfile

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware.js');
+const connectDB = require('./connect/database.js');
+const port = process.env.PORT || 5000;
+
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/tasks', require('./routes/taskRoutes.js'));
+app.use('/api/users', require('./routes/userRoutes.js'));
+
+app.use(errorHandler);
+app.listen(port, () => console.log(`Server listening on ${port}`));

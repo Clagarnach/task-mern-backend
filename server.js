6f57057646a5611c53c6995392a3c7ffa2055ae0
Clagarnach <olieclay@hotmail.com>
2024-08-15 18:58:02 +0100

    Add Cors

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware.js');
 const connectDB = require('./connect/database.js');
 const port = process.env.PORT || 5000;
-
+const Cors = require('cors');
+    
 connectDB();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(Cors());
 
 app.use('/api/tasks', require('./routes/taskRoutes.js'));
 app.use('/api/users', require('./routes/userRoutes.js'));
